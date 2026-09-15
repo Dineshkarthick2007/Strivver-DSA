07da95a5786ea3f77d1685d96cc1fa63fff595db
Dineshkarthick2007 <dineshkarthick122007@gmail.com>
2026-09-15 14:06:56 +0530

    Hashing

--- Main.java
+++ Main.java
@@ -1,19 +1,25 @@
 import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args)
+    {
         Scanner s = new Scanner(System.in);
-        String str = s.next();
-
-        int[] hash = new int[26];
-
-        for(int i=0;i<str.length;i++)
+        int n=s.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
         {
-            if(str[i]>='a' && str[i]<='z')
-            {
-                hash[str.charAt]
-            }
+            arr[i]=s.nextInt();
+        }
+        
+        Map<Integer,Integer> mp = new HashMap<>();
+
+        for(int i=0;i<n;i++)
+        {
+            mp.put(arr[i],mp.getOrDefault(arr[i], 0)+1);
+        }
+
+        for(int key:mp.keySet())
+        {
+            System.out.println(key+" -> "+mp.get(key));
         }
     }
 }
