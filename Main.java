dff92f45b2cbe777af30f690d4d1db3af7cc26da
Dineshkarthick2007 <dineshkarthick122007@gmail.com>
2026-09-17 19:46:52 +0530

    Frequency hashing

--- Main.java
+++ Main.java
@@ -3,23 +3,44 @@ public class Main{
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
-        int n=s.nextInt();
+        int n = s.nextInt();
+
         int[] arr = new int[n];
         for(int i=0;i<n;i++)
         {
             arr[i]=s.nextInt();
         }
-        
-        Map<Integer,Integer> mp = new HashMap<>();
+
+        Map<Integer,Integer> m = new HashMap<>();
 
         for(int i=0;i<n;i++)
         {
-            mp.put(arr[i],mp.getOrDefault(arr[i], 0)+1);
+            m.put(arr[i],m.getOrDefault(arr[i],0)+1);
         }
 
-        for(int key:mp.keySet())
+        int max=m.get(arr[0]);
+        int min=m.get(arr[0]);
+
+        int maxElement = arr[0];
+        int minElement = arr[0];
+
+        for(Map.Entry<Integer,Integer> entry : m.entrySet())
         {
-            System.out.println(key+" -> "+mp.get(key));
+            int element = entry.getKey();
+            int freq = entry.getValue();
+
+            if(freq>max)
+            {
+                max=freq;
+                maxElement=element;
+            }
+            if(freq<min)
+            {
+                min=freq;
+                minElement=element;
+            }
         }
+
+        System.out.print(maxElement+" "+minElement);
     }
 }
